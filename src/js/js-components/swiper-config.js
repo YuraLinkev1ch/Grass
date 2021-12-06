3a2caef478e9ba83c19682eee41a5c12ebf8b082
YuraLinkev1ch <yura.linkevich2012@gmail.com>
2021-12-06 20:41:17 +0300

    feat: add reviews section

--- src/js/js-components/swiper-config.js
+++ src/js/js-components/swiper-config.js
@@ -119,8 +119,6 @@ let mobileSwiperServices = new Swiper('.services__mobile-swiper', {
 
 /* services-swipers-end */
 
-/* swiper-config-end */
-
 
 
 /* portfolio-swipers-start */
@@ -144,3 +142,72 @@ let swiperPortfolio = new Swiper('.portfolio__swiper', {
 })
 
 /* portfolio-swipers-end */
+
+/* reviews-swipers-start */
+
+const breakpoint = window.matchMedia( '(max-width: 376px)' );
+    const breakpointChecker = function() {
+        if ( breakpoint.matches === true ) {
+            return initReviewsSwiper(breakpoint.matches);
+                } else if ( breakpoint.matches === false ) {
+            return initReviewsSwiper(breakpoint.matches);
+        }
+};
+
+var settings = {
+    // когда ширина окна меньше < 376px
+    modules: [Navigation, Pagination],
+    loop: 'true',
+    slidesPerView: 'auto',
+    slideClass: 'reviews__swiper-slide',
+    centeredSlides: true,
+
+    a11y: true,
+    keyboardControl: true,
+    grabCursor: true,
+
+    pagination: {
+        el: '.reviews__swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.reviews__swiper-button-next',
+        prevEl: '.reviews__swiper-button-prev',
+    },
+        breakpoints: {
+            // когда ширина окна больше или равна > 376px
+            376: {
+                centeredSlides: false,
+                pagination: {
+                    el: '.reviews__swiper-pagination',
+                    type: "fraction",
+                    renderFraction(currentClass, totalClass) {
+                        return `<span class="${currentClass}"></span>/<span class="${totalClass}"></span>`;
+                    },
+                    clickable: true,
+                },
+            },
+        },
+}
+
+var reviewsSwiper = new Swiper('.reviews__swiper', settings);
+
+const initReviewsSwiper = function(e) {
+    if ( e === true ) {
+        reviewsSwiper.destroy();
+        reviewsSwiper = new Swiper('.reviews__swiper', settings);
+    } else {
+        reviewsSwiper.destroy();
+        reviewsSwiper = new Swiper('.reviews__swiper', settings);
+    }
+}
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
+
+/* reviews-swipers-end *
+
+
+
+/* swiper-config-end */
