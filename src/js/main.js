a7ebdfb2566566ff562243b890a64cedb066d52a
YuraLinkev1ch <yura.linkevich2012@gmail.com>
2021-12-11 12:16:44 +0300

    feat: add contacts section

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,37 @@ portfolioArrowBtn.addEventListener('click', function() {
     this.classList.toggle('portfolio__arrow-btn_active');
     console.log('portfolioArrowBtn clicked!')
 });
+
+/* yandex-maps-config-start */
+
+let center = [53.944802, 27.351446];
+
+function init() {
+	let map = new ymaps.Map('contacts-map', {
+		center: center,
+		zoom: 16
+	});
+
+    let myIcon = ymaps.templateLayoutFactory.createClass('<div class="yandex-map__text">' + 
+        '{{ properties.iconCaption }}' + '</div>  <img class="yandex-map__image" src="./../img/marker.svg">');
+
+    let placemark = new ymaps.Placemark(center, {
+        iconCaption: 'Садовая улица, 23',
+    }, {
+        iconLayout: myIcon,
+	});
+
+	map.controls.remove('geolocationControl'); 
+    map.controls.remove('searchControl'); 
+    map.controls.remove('trafficControl'); 
+    map.controls.remove('typeSelector'); 
+    map.controls.remove('fullscreenControl'); 
+    map.controls.remove('zoomControl'); 
+    map.controls.remove('rulerControl'); 
+    map.behaviors.disable(['scrollZoom']); 
+    map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
+
+/* yandex-maps-config-end */
